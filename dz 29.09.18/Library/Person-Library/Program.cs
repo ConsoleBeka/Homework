eb8155e6c2b5e6446768e57acde3a68d8de747e0
ConsoleBeka <43833305+ConsoleBeka@users.noreply.github.com>
2018-11-19 18:02:53 +0600

    DZ 29.09.18

---
+++ dz 29.09.18/Library/Person-Library/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIrstLibrary;
+namespace Person_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+        }
+
+        static void FirstTask() {
+            FIrstLibrary.Person person = new FIrstLibrary.Person("He", "She", 18);
+
+            FIrstLibrary.Class1 firstClass = new Class1();
+            string str = firstClass.StaticMethod(person);
+            Console.WriteLine(str);
+        }
+
+        static void SecondTask() {
+            Book Lotr = new Book();
+            Console.WriteLine($"Название: {Lotr.GetName}");
+            Console.WriteLine($"Кол-во страниц: {Lotr.GetcountOfPages}");
+            Console.WriteLine($"Имя автора: {Lotr.GetauthorName}");
+        }
+    }
+}
