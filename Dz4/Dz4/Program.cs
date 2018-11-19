0369578a6bef909b2568fdd773e96d907e176823
ConsoleBeka <43833305+ConsoleBeka@users.noreply.github.com>
2018-11-19 11:12:17 +0600

    27.09.18 indexatory

---
+++ Dz4/Dz4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IndexArr indexArr = new IndexArr(2);
+            indexArr[0] = 5;
+            Console.WriteLine(indexArr[0]);
+            Console.ReadLine();
+        }
+    }
+}
